39328c6c53f5a4338c85977385a85b546b383527
a-marchenko <alexandr.marchenko@iss.ru>
2025-02-25 16:56:08 +0300

    #IPLUS-38152 add enums EApiResourceNames from ITS

---
+++ src/enums/EApiResourceNames.ts
@@ -0,0 +1,8 @@
+/**
+ * Имена корневых ресурсов API
+ */
+export enum EApiResourceNames {
+  INCIDENT_TYPES = '/its_api/its/new_incidents',
+  INCIDENT_LIST = '/its_api/its/incidents',
+  INCIDENTS_STATUSES_INFO = '/its_api/its/incidents_info',
+}
