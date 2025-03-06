fb5c53824b2e9a0068248c42ad010de728e29a04
a-marchenko <alexandr.marchenko@iss.ru>
2025-03-06 10:16:06 +0300

    #create table tab

---
+++ src/pages/MapPage/tabs/TableTab.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="table-tab">
+    Table tab coming soon
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.table-tab {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  font-family: Golos;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 32px; /* 133.333% */
+  color: var(--text-tertiary, #76879F);
+  letter-spacing: 0.24px;
+}
+</style>
