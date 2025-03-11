9b7f1db8aa6da43e1459e3a28556cafe559be393
a-marchenko <alexandr.marchenko@iss.ru>
2025-03-11 15:20:29 +0300

    #IPLUS-38371 create consts for Route names

---
+++ src/constants/routeNames.ts
@@ -0,0 +1,18 @@
+
+export const GENERAL_ROUTE_NAME = "GeneralView";
+
+export const PROFILE_ROUTE_NAME = "ProfilePage";
+
+export const AUTH_ROUTE_NAME = "AuthenticationPage";
+
+export const NOT_FOUND_ROUTE_NAME = "NotFound";
+
+export const MAP_ROUTE_NAME = "MapPage";
+
+export const ROUTE_NAMES = {
+  GENERAL_ROUTE_NAME,
+  PROFILE_ROUTE_NAME,
+  AUTH_ROUTE_NAME,
+  NOT_FOUND_ROUTE_NAME,
+  MAP_ROUTE_NAME
+};
